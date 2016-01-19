432aaa9a53bf59e0ac56074e348bf195d175ffc8
zhouxiaotian <1282347298@qq.com>
2016-01-19 17:56:35 +0800

    20160119_全日制第三周第一天下午-JS盒子偏移量和getCss兼容处理...

--- 20160119_全日制第三周第一天/js/utils.js
+++ 20160119_全日制第三周第一天/js/utils.js
@@ -14,3 +14,34 @@ var utils = {
         return "JSON" in window ? JSON.parse(str) : eval("(" + str + ")");
     }
 };
+
+utils.getCss = function (curEle, attr) {
+    var val = reg = null;
+    if ("getComputedStyle" in window) {
+        val = window.getComputedStyle(curEle, null)[attr];
+    } else {
+        if (attr === "opacity") {
+            val = curEle.currentStyle["filter"];
+            reg = /^alpha\(opacity=(\d+(\.\d+)?)\)$/;
+            val = reg.test(val) ? reg.exec(val)[1] / 100 : 1;
+        } else {
+            val = curEle.currentStyle[attr];
+        }
+    }
+    reg = /^-?\d+(\.\d+)?(px|em|rem|pt)?$/;
+    return reg.test(val) ? parseFloat(val) : val;
+};
+
+utils.offset = function (curEle) {
+    var t = curEle.offsetTop, l = curEle.offsetLeft, p = curEle.offsetParent;
+    while (p) {
+        if (navigator.userAgent.indexOf("MSIE 8.0") === -1) {
+            t += p.clientTop;
+            l += p.clientLeft;
+        }
+        t += p.offsetTop;
+        l += p.offsetLeft;
+        p = p.offsetParent;
+    }
+    return {top: t, left: l};
+};
