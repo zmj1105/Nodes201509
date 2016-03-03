f49e51d4684f832376a78cd2e2a5fd71fe5bd4fe
zhouxiaotian <1282347298@qq.com>
2016-03-03 18:20:05 +0800

    2016030303_全日制第七周内容-Ajax、JSONP、NODE、HTTP

--- 20160303/lib/server.js
+++ 20160303/lib/server.js
@@ -1,80 +1,94 @@
-//->导入依赖的模块
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
 
-
-//->创建一个server服务器
+var point = "1001";
 var server = http.createServer(function (request, response) {
-    var urlObject = url.parse(request.url, true);
-    var pathname = urlObject.pathname;
+    //->request.url获取的是客户端向服务器端请求的地址,包含了问号传递进来的参数
+    var urlObj = url.parse(request.url, true);
+    var pathname = urlObj.pathname;
+    var content = null;
 
-    //->访问的是adduser页面
+    //->如果客户端请求的是http://localhost:1001/index.html,我们把服务器的index.html文件返回给前端进行渲染
     if (pathname === "/index.html") {
-        var content = fs.readFileSync("./index.html", "utf8");
-        response.write(content);
-        response.end();
+        content = fs.readFileSync("." + pathname, "utf8");
+        response.writeHead(200, {'content-type': 'text/html'});
+        response.end(content);
         return;
     }
 
-    if (pathname === "/add") {
-        var query = urlObject.query;
-        content = fs.readFileSync("./userInfo.json", "utf8");
+    //->写我们数据请求的接口
+
+    //1)获取用户列表的接口
+    if (pathname === "/getInfo") {
+        content = fs.readFileSync("./userList.json", "utf8");
+        response.writeHead(200, {'content-type': 'application/json'});
+        response.end(content);
+        return;
+    }
+
+    //2)增加先的用户信息
+    if (pathname === "/addInfo") {
+        //->先把客户端发送给服务器的参数获取到
+        var userName = urlObj.query.userName;
+        var userAge = urlObj.query.userAge;
+
+        //->把数据存储到userList.json中
+        content = fs.readFileSync("./userList.json", "utf8");
         content = JSON.parse(content);
 
-        var n = content.count;
-        content.count = ++n;
+        content.count++;
+        var userId = content.count;
         content.list.push({
-            id: n,
-            userName: query.userName,
-            userAge: query.userAge
+            id: userId,//->新增加用户的ID和总数量是一致的
+            userName: userName,
+            userAge: userAge
         });
 
-        fs.writeFileSync("./userInfo.json", JSON.stringify(content), "utf8");
+        content = JSON.stringify(content);
+        fs.writeFileSync("./userList.json", content, "utf8");
 
-        var res = {
-            code: 0,
-            info: {
-                id: n,
-                userName: query.userName,
-                userAge: query.userAge
-            }
-        };
-
+        //->把当前用户的详细信息返回给前端
         response.writeHead(200, {'content-type': 'application/json'});
-        response.end(JSON.stringify(res));
+        response.end(JSON.stringify({
+            "id": userId,
+            "userName": userName,
+            "userAge": userAge
+        }));
         return;
     }
 
-    if (pathname === "/get") {
-        var con = fs.readFileSync("./userInfo.json", "utf8");
-        response.writeHead(200, {'content-type': 'application/json'});
-        response.end(con);
-        return;
-    }
+    //3)删除的接口
+    if (pathname === "/delInfo") {
+        userId = urlObj.query.id;
+        content = fs.readFileSync("./userList.json", "utf8");
+        content = JSON.parse(content);
 
-    if (pathname === "/del") {
-        query = urlObject.query;
-        con = fs.readFileSync("./userInfo.json", "utf8");
-        con = JSON.parse(con);
-        for (var i = 0; i < con.list.length; i++) {
-            if (con.list[i].id == query.id) {
-                con.count = con.count - 1;
-                con.list.splice(i, 1);
+        content.count--;
+        var ary = content.list;
+        for (var i = 0; i < ary.length; i++) {
+            if (ary[i].id == userId) {
+                ary.splice(i, 1);
                 break;
             }
         }
-        fs.writeFileSync("./userInfo.json", JSON.stringify(con), "utf8");
+
+        content = JSON.stringify(content);
+        fs.writeFileSync("./userList.json", content, "utf8");
+
         response.writeHead(200, {'content-type': 'application/json'});
-        response.end(JSON.stringify({code: 0}));
+        response.end(JSON.stringify({
+            "code": 0,
+            "message": "删除成功"
+        }));
         return;
     }
+
+    //->如果不是以上的地址访问,让浏览器渲染404页面
+    response.writeHead(404, {'content-type': 'text/html'});
+    response.end(fs.readFileSync("./error.html", "utf8"));
 });
-
-//->监听一个端口请求
-server.listen(1234, function () {
-    console.log("启动成功,正在监听1234这个端口");
+server.listen(point, function () {
+    console.log("当前 [ " + point + " ] 端口已经开启!");
 });
-
-
 
